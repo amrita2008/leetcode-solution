fb3c023dd6557f64c894efab6a61a82b3965186f
Amrita <amritavaish16@gmail.com>
2026-01-01 19:16:52 +0530

    Time: 2 ms (40.6%), Space: 8.3 MB (15.82%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string result;
+        for(int i=0;i<command.size();){
+            if(command[i]=='G') result+='G',i++;
+            else if(command[i+1]==')') result+='o',i+=2;
+            else result += "al", i+=4;
+        }
+        return result;
+    }
+};
