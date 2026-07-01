c2df36783a3171b48d07d22c25cf4b1879420914
Amrita <amritavaish16@gmail.com>
2026-07-01 14:46:51 +0530

    Time: 0 ms (100%), Space: 23 MB (83.35%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,32 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    pair<bool,int> fast(TreeNode* &root){
-        if(root == NULL){
-            return {true,0};
-        }
-        pair<bool,int> left = fast(root->left);
-        pair<bool,int> right = fast(root->right);
+    bool balanced = true;
 
-        bool left1 = left.first;
-        bool right1 = right.first;
-        bool curr = abs(left.second-right.second)<=1;
-        pair<bool,int> ans;
-        ans.second = max(left.second,right.second)+1;
-        ans.first = curr&&left1&&right1;
-        return ans;
+    int dfs(TreeNode* node) {
+        if (node == nullptr)
+            return 0;
+
+        int l = dfs(node->left);
+        int r = dfs(node->right);
+
+        if (abs(l - r) > 1)
+            balanced = false;
+
+        return max(l, r) + 1;
     }
+
     bool isBalanced(TreeNode* root) {
-        return fast(root).first;
+        dfs(root);
+        return balanced;
     }
 };
